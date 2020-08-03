bcaedc6bde0d73ae9708de17cb2ea7a14ca32be1
Kelvin <64967043+kelvinjang@users.noreply.github.com>
2020-08-04 00:51:58 +0900

    Add files via upload

--- conv.py
+++ conv.py
@@ -1,110 +1,143 @@
-# import configparser
 import datetime
 import pathlib
 import sqlite3
 import sys
 
 
+
 def welcome():
 
-    # Welcome message
     print("------------------------------")
-    print(" Welcome to Convtracker v0.01 ")
+    print(" Welcome to Convtracker v0.10 ")
     print("------------------------------")
 
 
 
-def main(sql, cur):
+def main(newdb, sql, cur):
+
+    # Todo: Redesign based on checking tables instead of newdb
+    if newdb == True:
+        print("- Database does not contain any characters. Please add a character.\n")
+        addchar(sql, cur)
 
     while True:
-        menu = getstr(">> Conversion Tracker\n[1]= View, [2]= Add entry, [3]= Modify entry, [4]= New database, [5]= Export, [Q]= Cancel: ").lower()
+        menu = getstr(">> Conversion Tracker\n[1]= View, [2]= Add/Update, [3]= Import/Export, [4]= Add character, [Q]= Cancel: ").lower()
 
 
         if menu == "1":
             print("\n>> View")
-            charname = getstr("Main character name: ").lower()
+            charname = getstr("Character Name: ").lower()
 
             if len(charname) <= 12 and len(charname) > 0:
                 tablename = charname + "_conv"
-
-                action = """SELECT avg(armor), count(armor), sum(armor), avg(wtd), count(wtd), sum(wtd), avg(acc), count(acc), sum(acc) FROM """ + tablename
+                action = """SELECT avg(armor), avg(wtd), avg(acc), sum(armor), sum(wtd), sum(acc), count(armor), count(wtd), count(acc) FROM """ + tablename + """;"""
 
                 try:
                     cur.execute(action)
-                    view = cur.fetchall()
+                    data = cur.fetchall()
 
-                    ocount = (view[0])[1] + (view[0])[4] + (view[0])[7]
-                    ototal = (view[0])[2] + (view[0])[5] + (view[0])[8]
-                    oavg = round(ototal / ocount, 1)
+                    if (data[0])[0] != None and (data[0])[1] != None and (data[0])[2] != None and (data[0])[3] != None and (data[0])[4] != None and (data[0])[5] != None:
 
-                    print("")
-                    print("[Armor] Avg:" + str(round((view[0])[0], 1)) + ", Count:" + str((view[0])[1]) + ", Total:" + str((view[0])[2]))
-                    print("  [Wtd] Avg:" + str(round((view[0])[3], 1)) + ", Count:" + str((view[0])[4]) + ", Total:" + str((view[0])[5]))
-                    print("  [Acc] Avg:" + str(round((view[0])[6], 1)) + ", Count:" + str((view[0])[7]) + ", Total:" + str((view[0])[8]))
-                    print("  [All] Avg:" + str(oavg) + ", Count:" + str(ocount) + ", Total:" + str(ototal))
+                        ototal = (data[0])[3] + (data[0])[4] + (data[0])[5]
+                        ocount = (data[0])[6] + (data[0])[7] + (data[0])[8]
+                        oavg = round(ototal / ocount, 1)
+
+                        print("")
+                        print("[Armor] Avg:" + str(round((data[0])[0], 1)) + ", Count:" + str((data[0])[6]) + ", Total:" + str((data[0])[3]))
+                        print("  [Wtd] Avg:" + str(round((data[0])[1], 1)) + ", Count:" + str((data[0])[7]) + ", Total:" + str((data[0])[4]))
+                        print("  [Acc] Avg:" + str(round((data[0])[2], 1)) + ", Count:" + str((data[0])[8]) + ", Total:" + str((data[0])[5]))
+                        print("  [All] Avg:" + str(oavg) + ", Count:" + str(ocount) + ", Total:" + str(ototal))
+
+                    else:
+                        print("- Data is missing or invalid.")
 
                 except sqlite3.Error:
-                    print("* Error: Character not found.")
+                    print("- Character not found.")
 
             print("")
             continue
 
 
+        # Todo: Handle no character earlier
         elif menu == "2":
-            print("\n>> Add entry")
-            charname = getstr("Main character name: ").lower()
+            print("\n>> Add/Update")
+            charname = getstr("Character Name: ").lower()
 
             if len(charname) <= 12 and len(charname) > 0:
                 tablename = charname + "_conv"
                 date = datetime.datetime.now().strftime("%x")
+                dateindb = False
+                proceed = False
 
-                print("\nValues for " + date + ":")
-                armor = getint("Armor: ")
-                wtd = getint("Wtd: ")
-                acc = getint("Acc: ")
+                keepdate = getstr("\n- Add/Update data for today (" + date + ")?\n[Y]= Yes, [N]=Select another date: ").lower()
 
-                if armor != "" and wtd != "" and acc != "":
-                    action = """INSERT INTO """ + tablename + """ (date, armor, wtd, acc) VALUES (?, ?, ?, ?)"""
+                if keepdate == "y":
+                    proceed = True
+
+                elif keepdate == "n":
+                    month = getdate("Month (MM): ")
+                    if month != "":
+                        day = getdate("Day (DD): ")
+                        if day != "":
+                            year = getdate("Year (YY): ")
+
+                    if month != "" and day != "" and year != "":
+                        date = month + "/" + day + "/" + year
+                        proceed = True
+
+                if proceed == True:
+                    action = """SELECT rowid FROM """ + tablename + """ WHERE date = ?;"""
+                    modifier = ""
 
                     try:
-                        cur.execute(action, (date, armor, wtd, acc))
-                        sql.commit()
-                        print("* Added to database.")
+                        cur.execute(action, (date,))
+                        checkdate = cur.fetchone()
+                        if checkdate is not None:
+                            print("\n- Existing data for " + date + " found.")
+                            dateindb = True
+                            modifier = "new "
+                    except sqlite3.Error as e:
+                        print(e)
 
-                    except sqlite3.Error:
-                        print("* Error: Data has already been updated today or character does not exist.")
+                    print("\nEnter " + modifier + "values for " + date + ":")
+                    armor = getint("Armor: ")
+                    if armor != "":
+                        wtd = getint("Wtd: ")
+                        if wtd != "":
+                            acc = getint("Acc: ")
+
+                    if armor != "" and wtd != "" and acc != "":
+                        if dateindb == False:
+                            action = """INSERT INTO """ + tablename + """ (date, armor, wtd, acc) VALUES (?, ?, ?, ?);"""
+                            try:
+                                cur.execute(action, (date, armor, wtd, acc))
+                                sql.commit()
+                                print("- Saved.")
+                            except sqlite3.Error as e:
+                                print(e)
+
+                        elif dateindb == True:
+                            action = """UPDATE """ + tablename + """ SET armor = ?, wtd = ?, acc = ? WHERE date = ?;"""
+                            try:
+                                cur.execute(action, (armor, wtd, acc, date))
+                                sql.commit()
+                                print("- Saved.")
+                            except sqlite3.Error as e:
+                                print(e)
 
             print("")
             continue
 
 
+        # Todo: Csv handling
         elif menu == "3":
             print("")
             continue
 
 
         elif menu == "4":
-            print("\n>> New database")
-            charname = getstr("Main character name: ").lower()
-
-            if len(charname) <= 12 and len(charname) > 0:
-                tablename = charname + "_conv"
-                action = """CREATE TABLE """ + tablename + """ (date NUMERIC NOT NULL PRIMARY KEY, armor INTEGER NOT NULL, wtd INTEGER NOT NULL, acc INTEGER NOT NULL)"""
-
-                try:
-                    cur.execute(action)
-                    sql.commit()
-                    print("* Added to database.")
-
-                except sqlite3.Error:
-                    print("* Error: Character already in database.")
-
-            print("")
-            continue
-
-
-        elif menu == "5":
-            print("")
+            print("\n>> Add character")
+            addchar(sql, cur)
             continue
 
 
@@ -112,105 +145,48 @@ def main(sql, cur):
             print("\nGoodbye.")
             break
 
+
         else:
             print("* Invalid input.\n")
             continue
-
 
     # Closes connection
     sql.close()
 
 
 
-"""
-# Ini/Config checker
-def inicheck():
+def addchar(sql, cur):
 
-    # Defines ini path + local variables for checking ini and db
-    inipath = pathlib.Path('config.ini')
-    configexists = False
-    configreset = False
-    newdbstatus = False
+    charname = getstr("Character Name: ").lower()
 
-    # Triggers configreset if ini file is missing
-    if inipath.is_file() == False:
-        configreset = True
+    if len(charname) <= 12 and len(charname) > 0:
+        tablename = charname + "_conv"
+        action = """CREATE TABLE """ + tablename + """ (date NUMERIC NOT NULL PRIMARY KEY, armor INTEGER NOT NULL, wtd INTEGER NOT NULL, acc INTEGER NOT NULL);"""
+        try:
+            cur.execute(action)
+            sql.commit()
+            print("- Character added.")
 
-    # Triggers configexists + configreset if ini file exists but keys and/or values are invalid
-    else:
-        configcheck = configparser.ConfigParser()
-        configcheck.read("config.ini")
+        except sqlite3.Error:
+            print("- Character already in database.")
 
-        if configcheck.has_option("Settings", "convdb") == False or configcheck.get("Settings","convdb") == "" or configcheck.get("Settings","convdb").endswith(".db") == False:
-            configexists = True
-            configreset = True
-        else:
-            db = configcheck.get("Settings","convdb")
-
-    # Resets ini file
-    if configreset == True:
-
-        # Requests permission from user if file already exists
-        if configexists == True:
-            print("Configuration error detected. Reset to default configuration?")
-            yesno()
-            print("")
-
-        while True:
-            dbname = getstr("Enter a name for your conversion database: ")
-
-            if len(dbname) <= 10 and len(dbname) > 0:
-                configset = configparser.ConfigParser()
-                configset.add_section("Settings")
-                configset.set("Settings","convdb", dbname + ".db")
-
-                ini = open("config.ini", "w")
-                configset.write(ini)
-                ini.close()
-
-                db = dbname + ".db"
-                newdbstatus = True
-
-                print("* Configuration saved to 'config.ini'.\n")
-                break
-
-            elif dbname == "":
-                print("* No input detected. Try again?")
-                yesno()
-                print("")
-                continue
-
-            else:
-                print("* Database name must be 1 to 10 alphanumeric characters long.\n")
-                continue
-
-    return db, newdbstatus
-    """
+    print("")
 
 
 
 # Db checker
 def dbcheck():
 
-    # Creates new db file if missing
-    db = "conv.db"
+    db = "datanest.db"
     dbpath = pathlib.Path(db)
+    newdb = False
 
     if dbpath.is_file() == False:
-
-        """
-        if newdbstatus == False:
-            print("Database not found. Create new database '" + db + "'?")
-        else:
-        """
-
-        print("Create new database '" + db + "'?")
-        yesno()
-
         open(db, "w").close()
-        print("* Database created.\n")
+        newdb = True
+        print("- Database not found. New database '" + db + "' created.")
 
-    return db
+    return db, newdb
 
 
 
@@ -218,7 +194,6 @@ def dbcheck():
 def sqlconnect(db):
 
     # Todo: Handle failed connnections
-    # Connects to db file
     sql = sqlite3.connect(db)
     cur = sql.cursor()
 
@@ -233,7 +208,7 @@ def yesno():
         try:
             response = input("[Y]= Yes, [Q]= No/Quit: ").lower()
         except:
-            print("\n* Please select a valid option.")
+            print("\n- Please select a valid option.")
             continue
         if response == "y":
             break
@@ -241,7 +216,7 @@ def yesno():
             print("\nGoodbye.")
             sys.exit(1)
         else:
-            print("\n* Please select a valid option.")
+            print("\n- Please select a valid option.")
             continue
 
 
@@ -252,16 +227,15 @@ def getstr(x):
         try:
             s = input(x)
         except:
-            print("* Please use alphanumeric characters.\n")
+            print("- Please use alphanumeric characters.\n")
             continue
         if s.isalnum() == True:
             break
         elif s == "":
             break
         else:
-            print("* Please use alphanumeric characters.\n")
+            print("- Please use alphanumeric characters.\n")
             continue
-
     return s
 
 
@@ -272,22 +246,40 @@ def getint(x):
         try:
             i = input(x)
         except:
-            print("* Please key in a number.")
+            print("- Please key in a number.")
             continue
         if i.isdigit() == True:
             break
         elif i == "":
             return str(i)
         else:
-            print("* Please key in a number.")
+            print("- Please key in a number.")
             continue
-
     return int(i)
+
+
+
+def getdate(x):
+
+    while True:
+        try:
+            date = input(x)
+        except:
+            print("- Please key in a 2 digit number.")
+            continue
+        if date.isdigit() == True and len(date) == 2:
+            break
+        elif date == "":
+            break
+        else:
+            print("- Please key in a 2 digit number.")
+            continue
+    return date
 
 
 
 welcome()
 # db, newdbstatus = inicheck()
-db = dbcheck()
+db, newdb = dbcheck()
 sql, cur = sqlconnect(db)
-main(sql, cur)
+main(newdb, sql, cur)
